43f5961df8a8d901858846a4cdaee3c0c57433d6
Graham Gilbert <graham.gilbert@airbnb.com>
2024-08-14 20:58:05 -0700

    Spacing

--- lib/puppet/type/munki_package.rb
+++ lib/puppet/type/munki_package.rb
@@ -12,6 +12,7 @@ Puppet::Type.newtype(:munki_package) do
   newparam(:name, :namevar => true) do
     desc 'The package name as known to Munki'
   end
+  
   newparam(:catalog) do
      desc 'The catalog from which we will get the package'
      defaultto 'production'
